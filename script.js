e926fb3e0f00e350c751db99216fee5524ed8723
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-02-25 11:54:50 +0300

    Added function to display weather icon

--- script.js
+++ script.js
@@ -86,3 +86,8 @@ function displayHourlyForecast(hourlyData) {
     hourlyForecastDiv.innerHTML += hourlyItemHtml;
   });
 }
+
+function showImage() {
+  const weatherIcon = document.getElementById("weather-icon");
+  weatherIcon.style.display = "block"; // Make the image visible once it's loaded
+}
